2f0bfbfb862459b766be5f8b95b3c7b4f383efed
vanmoscoder <mosvan00@gmail.com>
2025-08-28 11:19:09 +0300

    server.js

--- server.js
+++ server.js
@@ -8,19 +8,41 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
-// When a user connects
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
+// Track who is typing
+const typingUsers = new Set();
 
-  // When they send a message
-  socket.on('chat message', (msg) => {
-    console.log('Message from', socket.id, ':', msg);
-    // Broadcast to everyone *else* (not back to self)
-    socket.broadcast.emit('chat message', msg);
+io.on('connection', (socket) => {
+  console.log('User connected:', socket.id);
+
+  // When user starts typing
+  socket.on('typing', (username) => {
+    if (username) {
+      typingUsers.add(username);
+      socket.broadcast.emit('update typing', Array.from(typingUsers));
+    }
   });
 
-  // When they disconnect
+  // When user stops typing
+  socket.on('stop typing', (username) => {
+    if (username) {
+      typingUsers.delete(username);
+      socket.broadcast.emit('update typing', Array.from(typingUsers));
+    }
+  });
+
+  // Chat message
+  socket.on('chat message', (data) => {
+    io.emit('chat message', data); // Send to everyone
+  });
+
+  // On disconnect
   socket.on('disconnect', () => {
+    // Remove user from typing list if they disconnect
+    for (let user of typingUsers) {
+      if (user.includes(socket.id) || user === /* you can't know username here, so client handles */ ) {
+        // We’ll clean up on frontend
+      }
+    }
     console.log('User disconnected:', socket.id);
   });
 });
