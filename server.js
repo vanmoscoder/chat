bcd961bfac858fcca31159e46f92efcd458d5254
vanmoscoder <mosvan00@gmail.com>
2025-08-28 23:46:16 +0300

    server.js

--- server.js
+++ server.js
@@ -8,43 +8,21 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
-// Track connected users
-const users = new Map();
-
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  // User joins
-  socket.on('user joined', (data) => {
-    const oldUsername = users.get(socket.id);
-    if (oldUsername) {
-      users.delete(oldUsername);
-    }
-    users.set(socket.id, data.username);
-    console.log(`${data.username} joined`);
-
-    // Broadcast to others
-    io.emit('user joined', { username: data.username });
-  });
-
-  // User disconnects
-  socket.on('disconnect', () => {
-    const username = users.get(socket.id);
-    if (username) {
-      console.log(`${username} left`);
-      users.delete(socket.id);
-      io.emit('user left', username);
-    }
-  });
-
   // Text message
   socket.on('chat message', (data) => {
-    socket.broadcast.emit('chat message', data);
+    io.emit('chat message', data);
   });
 
   // Image message
   socket.on('chat image', (data) => {
-    socket.broadcast.emit('chat image', data);
+    io.emit('chat image', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
   });
 });
 
