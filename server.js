cf29d57aa84cd0a1c6759ca6e05c56034588a63d
vanmoscoder <mosvan00@gmail.com>
2025-08-28 09:40:21 +0300

    server.js

--- server.js
+++ server.js
@@ -8,36 +8,20 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
-// Track connected users (optional, for future use)
-const connectedUsers = new Set();
-
+// When a user connects
 io.on('connection', (socket) => {
-  console.log('New connection:', socket.id);
+  console.log('A user connected:', socket.id);
 
-  // When a user sends their username upon joining
-  socket.on('user joined', (username) => {
-    socket.username = username; // Store username in socket
-    connectedUsers.add(username);
-    // Broadcast to others
-    socket.broadcast.emit('system message', `${username} joined the chat`);
+  // When they send a message
+  socket.on('chat message', (msg) => {
+    console.log('Message from', socket.id, ':', msg);
+    // Broadcast to everyone *else* (not back to self)
+    socket.broadcast.emit('chat message', msg);
   });
 
-  // When a user sends a chat message
-  socket.on('chat message', (data) => {
-    // Send to everyone (including sender, but we handle UI cleanly)
-    io.emit('chat message', data);
-  });
-
-  // When user disconnects
+  // When they disconnect
   socket.on('disconnect', () => {
-    if (socket.username) {
-      console.log('User disconnected:', socket.username);
-      connectedUsers.delete(socket.username);
-      // Tell others
-      socket.broadcast.emit('system message', `${socket.username} left the chat`);
-    } else {
-      console.log('Anonymous user disconnected');
-    }
+    console.log('User disconnected:', socket.id);
   });
 });
 
