d96b18bef683eed676701b1fcb7af4b4c0897e41
vanmoscoder <mosvan00@gmail.com>
2025-09-01 22:28:01 +0300

    server.js

--- server.js
+++ server.js
@@ -8,45 +8,20 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
-const users = {}; // Track connected users
-
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  // Register user
-  socket.on('register', (username) => {
-    users[socket.id] = username;
-    socket.broadcast.emit('users', Object.values(users));
-  });
-
-  // Public message
+  // Send text message to others only
   socket.on('chat message', (data) => {
-    socket.broadcast.emit('chat message', data);
+    socket.broadcast.emit('chat message', data); // Not back to self
   });
 
+  // Send image to others only
   socket.on('chat image', (data) => {
-    socket.broadcast.emit('chat image', data);
+    socket.broadcast.emit('chat image', data); // Not back to self
   });
 
-  // Private message
-  socket.on('private message', (data) => {
-    const recipientId = Object.keys(users).find(id => users[id] === data.to);
-    if (recipientId) {
-      io.to(recipientId).emit('private message', data);
-    }
-  });
-
-  socket.on('private image', (data) => {
-    const recipientId = Object.keys(users).find(id => users[id] === data.to);
-    if (recipientId) {
-      io.to(recipientId).emit('private image', data);
-    }
-  });
-
-  // Disconnect
   socket.on('disconnect', () => {
-    delete users[socket.id];
-    io.emit('users', Object.values(users));
     console.log('User disconnected:', socket.id);
   });
 });
