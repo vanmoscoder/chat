7dfc933a6acaddbd82bfa94993a30a008d6a8512
vanmoscoder <mosvan00@gmail.com>
2025-09-18 20:04:58 +0300

    server.js

--- server.js
+++ server.js
@@ -8,71 +8,19 @@ const io = socketIo(server);
 
 app.use(express.static('public'));
 
-// Global state
-let users = []; // All users who ever joined
-let groups = {}; // { groupId: { name, members: [] } }
-
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  // When a user joins
-  socket.on('user joined', (data) => {
-    const { username } = data;
-    if (!users.includes(username)) {
-      users.push(username);
-    }
-    // Broadcast to everyone that a user joined
-    io.emit('user joined', { username });
-  });
-
-  // Send message to group or general
+  // Send text message to others only
   socket.on('chat message', (data) => {
-    const { user, message, reply, groupId } = data;
-    if (groupId) {
-      // If it's a group message, broadcast only to group members
-      const group = groups[groupId];
-      if (group && group.members.includes(user)) {
-        io.emit('chat message', { user, message, reply, groupId });
-      }
-    } else {
-      // General chat — broadcast to all
-      io.emit('chat message', { user, message, reply });
-    }
+    socket.broadcast.emit('chat message', data); // Not back to self
   });
 
-  // Send image to group or general
+  // Send image to others only
   socket.on('chat image', (data) => {
-    const { user, image, reply, caption, groupId } = data;
-    if (groupId) {
-      const group = groups[groupId];
-      if (group && group.members.includes(user)) {
-        io.emit('chat image', { user, image, reply, caption, groupId });
-      }
-    } else {
-      io.emit('chat image', { user, image, reply, caption });
-    }
+    socket.broadcast.emit('chat image', data); // Not back to self
   });
 
-  // Delete message
-  socket.on('delete message', (data) => {
-    const { messageId } = data;
-    io.emit('message deleted', { messageId });
-  });
-
-  // Create group
-  socket.on('create group', (data) => {
-    const { name, members } = data;
-    const groupId = Date.now().toString();
-    groups[groupId] = {
-      name,
-      members,
-      createdAt: Date.now()
-    };
-    // Notify everyone of new group
-    io.emit('group created', { groupId, name, members });
-  });
-
-  // Disconnect
   socket.on('disconnect', () => {
     console.log('User disconnected:', socket.id);
   });
